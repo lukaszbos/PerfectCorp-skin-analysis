afed53813d7161cb4a33db54b661c77dc0820b0a
Łukasz <38592150+lukaszbos@users.noreply.github.com>
2025-05-24 10:53:51 +0200

    Update run_skin_analysis.py

--- run_skin_analysis.py
+++ run_skin_analysis.py
@@ -5,12 +5,13 @@ import os
 from datetime import datetime
 
 API_HOST = "yce-api-01.perfectcorp.com"
-AUTH_TOKEN = "Bearer "
-POLL_INTERVAL_MS = 50
+AUTH_TOKEN = "Bearer AgcErORSAAAAFnljZTozMTgzMjQ2OTI1OTczNDQwNTMAAAAAAAAAAQRq9F0KQAmGAAAAAAIAAAAAAAAAAAAAAAA=.UDeGaKsuioSOp3nUhVIxKa+z6tg="
+POLL_INTERVAL_MS = 1000
 MAX_RETRIES = 120
 
 
 def create_new_file_obtain_file_id(image_path):
+    print(f"Start File creation!")
     file_name = os.path.basename(image_path)
     file_size = os.path.getsize(image_path)
     payload = json.dumps({
@@ -35,7 +36,26 @@ def create_new_file_obtain_file_id(image_path):
 
     response_json = json.loads(data)
     file_id = response_json["result"]["files"][0]["file_id"]
+    url_to_upload = response_json["result"]["files"][0]["requests"][0]["url"]
+    headers_to_upload = response_json["result"]["files"][0]["requests"][0]["headers"]
+    content_length = headers_to_upload["Content-Length"]
+    content_type = headers_to_upload["Content-Type"]
     print(f"File Created! Extracted file_id: {file_id}")
+    
+
+    with open(image_path, "rb") as image_file:
+        file_data = image_file.read()  # Read the file as bytes
+        conn2 = http.client.HTTPSConnection("yce-us.s3-accelerate.amazonaws.com")
+        print(f"Url path {url_to_upload}")
+        url_path = url_to_upload.split("yce-us.s3-accelerate.amazonaws.com")[1]
+        print(f"Url path {url_path}")
+        conn2.request("PUT", url_path, body=file_data, headers=headers_to_upload)
+        res = conn2.getresponse()
+        print(f"File Uploaded {file_id}")
+        print(res.status, res.reason)
+    
+    
+    
     return file_id
 
 
@@ -100,7 +120,7 @@ def poll_task_status(task_id):
 
 
 if __name__ == "__main__":
-    image_path = r"C:\Users\lukas\Desktop\Skinally-2025\PerfectCorpAPI\no-makeup.png"
+    image_path = r"C:\Users\lukas\Desktop\Skinally-2025\PerfectCorpAPI\mojafota-1900x1400.png"
     file_id = create_new_file_obtain_file_id(image_path)
     task_id = run_skin_analisys_obtain_task_id(file_id)
     result = poll_task_status(task_id)
